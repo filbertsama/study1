bad3ff358ff00893419ac9e377d7b35e3ac012eb
filbert <lyzhaha.love@gmail.com>
2023-02-24 16:40:13 +0800

    test0

---
+++ src/main/java/com/filbert/Test2.java
@@ -0,0 +1,9 @@
+package com.filbert;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("kaka");
+        System.out.println("mama");
+        System.out.println("wdc");
+    }
+}
