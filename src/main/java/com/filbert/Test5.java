abde2fc77b1a51f4adcc6abd294e6e816bf5c8d4
filbert <lyzhaha.love@gmail.com>
2023-02-24 16:33:07 +0800

    test12

--- src/main/java/com/filbert/Test5.java
+++ src/main/java/com/filbert/Test5.java
@@ -4,5 +4,11 @@ public class Test5 {
     public static void main(String[] args) {
 
         System.out.println("你好，这是测试");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("");
+            }
+        }).start();
     }
 }
